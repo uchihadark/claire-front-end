6bd9239fe587fd3bc39003781007f1e04f94d792
sourav <anime.singh8950@gmail.com>
2021-01-10 18:39:57 +0530

    improving styles

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,7 +4,7 @@ const Rank = ({ name, entries }) => {
   return (
     <div>
       <div className='white f3'>
-        {`${name}, The Total Number Of Images which you have submitted...`}
+        {`${name},the total number of images which you have submitted...`}
       </div>
       <div className='white f1'>
         {entries}
